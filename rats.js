805cd2d7879101335488ad5939fd8be2f177de7a
Elijah Collins <elishenlong@gmail.com>
2020-10-21 14:00:25 -0700

    Adding Player Page to try and use API on first

--- rats.js
+++ rats.js
@@ -1,8 +1,8 @@
 //////Use DOM Selection to select items we want to change with AJAX////////////
-const starter1 = document.querySelector('.starter1');
-const starter1img = document.querySelector('.starter1img');
-const cardText = document.querySelector('.card-text');
-const recent = document.querySelector('.text-muted')
+const () = document.querySelector('.starter1');
+const ()img = document.querySelector('.starter1img');
+const () = document.querySelector('.card-text');
+const () = document.querySelector('.text-muted')
 ////////Create our Request to serve1
 let xhr = new XMLHttpRequest();
 ////////Set up a callback function to run when server req is successful///////////
@@ -14,10 +14,10 @@ xhr.onreadystatechange = () => {
         ///////Body of the callback where we parse data and use DOM Manipulation//////////
         const data = JSON.parse(xhr.response);
         console.log(data);
-        starter1.innerHTML = data.name;
-        starter1img.src = data.sprites.front_default;
-        cardText.innerHTML = data.abilities[0].ability.name;
-        recent.innerHTML = data.game_indices[19].version.name;
+        ().innerHTML = data.name;
+        ().src = data.sprites.front_default;
+        ().innerHTML = data.abilities[0].ability.name;
+        ().innerHTML = data.game_indices[19].version.name;
         ///////Else if clause where we throw an error if we dont have success connecting//
     } else if(xhr.status === 404) {
         alert("The page you seem to be looking for cannot be found.");
@@ -26,6 +26,6 @@ xhr.onreadystatechange = () => {
     }
 };
 ///////open server request////////////////////////////////////////////////////////
-xhr.open("GET", "openapi.json", "openapi-2.json");
+xhr.open("GET", "api/openapi.json", "api/openapi-2.json");
 ///////send server request////////////////////////////////////////////////////////
 xhr.send();
