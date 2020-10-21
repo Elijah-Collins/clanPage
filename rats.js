28a40a8ec1ceaaac6c7a6e21077a0eb2714dc18f
Elijah Collins <elishenlong@gmail.com>
2020-10-21 14:52:29 -0700

    changing and updating page and js content

--- rats.js
+++ rats.js
@@ -1,8 +1,7 @@
 //////Use DOM Selection to select items we want to change with AJAX////////////
-const () = document.querySelector('.starter1');
-const ()img = document.querySelector('.starter1img');
-const () = document.querySelector('.card-text');
-const () = document.querySelector('.text-muted')
+const playerimg = document.querySelector('.starter1img');
+const cardText = document.querySelector('.card-text');
+const cardTitle = document.querySelector('.card-title')
 ////////Create our Request to serve1
 let xhr = new XMLHttpRequest();
 ////////Set up a callback function to run when server req is successful///////////
@@ -14,10 +13,9 @@ xhr.onreadystatechange = () => {
         ///////Body of the callback where we parse data and use DOM Manipulation//////////
         const data = JSON.parse(xhr.response);
         console.log(data);
-        ().innerHTML = data.name;
-        ().src = data.sprites.front_default;
-        ().innerHTML = data.abilities[0].ability.name;
-        ().innerHTML = data.game_indices[19].version.name;
+        playerimg.src = data.sprites.front_default;
+        cardTitle.innerHTML = data.name;
+        cardText.innerHTML = data.abilities[0].ability.name;
         ///////Else if clause where we throw an error if we dont have success connecting//
     } else if(xhr.status === 404) {
         alert("The page you seem to be looking for cannot be found.");
