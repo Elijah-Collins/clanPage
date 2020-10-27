0fdc90cc2e35681b0947b1b904e7ec70a7018468
Elijah Collins <ecollins152@west-mec.org>
2020-10-27 15:07:41 -0700

    api thing does api

--- rats.js
+++ rats.js
@@ -5,7 +5,7 @@ const cardTitle = document.querySelector('.card-title')
 ////////Create our Request to serve1
 let xhr = new XMLHttpRequest();
 ////////Set up a callback function to run when server req is successful///////////
-xhr.onreadystatechange = () => {
+xhr.onload = () => {
     ///////Create an if statement that asks if the server req is complete/////////////
     if(xhr.readyState === 4)
     ///////now server is complete create an if to see if connection succcessful///////
@@ -13,9 +13,6 @@ xhr.onreadystatechange = () => {
         ///////Body of the callback where we parse data and use DOM Manipulation//////////
         const data = JSON.parse(xhr.response);
         console.log(data);
-        playerimg.src = data.sprites.front_default;
-        cardTitle.innerHTML = data.name;
-        cardText.innerHTML = data.abilities[0].ability.name;
         ///////Else if clause where we throw an error if we dont have success connecting//
     } else if(xhr.status === 404) {
         alert("The page you seem to be looking for cannot be found.");
@@ -24,6 +21,9 @@ xhr.onreadystatechange = () => {
     }
 };
 ///////open server request////////////////////////////////////////////////////////
-xhr.open("GET", "https://bungie-net.github.io");
+xhr.open("GET", "https://www.bungie.net/Platform/Destiny2/Manifest/");
+xhr.setRequestHeader("X-API-Key", "c019e29561f549c2839a986482d80140", "Access-Control-Allow-Origin", "http://www.bungie.net",);
 ///////send server request////////////////////////////////////////////////////////
 xhr.send();
+
+// Access-Control-Allow-Origin: http://www.bungie.net
